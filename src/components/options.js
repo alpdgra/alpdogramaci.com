fc201fa5d12e2c4a28c0740325fdc1054f8bc23b
Micky <60691199+AliMickey@users.noreply.github.com>
2024-11-24 00:42:18 +1100

    remove particle

--- src/components/options.js
+++ src/components/options.js
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from "react";
-import Particles from "react-particles-js";
-import { ParticlesParams } from "../schemas/particles";
 
 export const Options = () => {
   var { lsTheme, lsIcon, lsSnow } = "";
@@ -23,10 +21,6 @@ export const Options = () => {
     document.body.classList[theme === "dark" ? "add" : "remove"]("dark-theme");
   }, [theme, snow, icon]);
 
-  const SnowEffect = () =>
-    snow && theme === "dark" && <Particles params={ParticlesParams}/>;
-
-  const _enableSnow = () => setSnow(!snow);
   const _toggleTheme = () => {
     theme === "light" ? setTheme("dark") : setTheme("light");
     icon === "bx-sun" ? setIcon("bx-moon") : setIcon("bx-sun");
